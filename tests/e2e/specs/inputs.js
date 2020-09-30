177de46e11b6d1f5abd1ba18d78ffd6f76b7ec36
Kevin Old <kevin@cypress.io>
2020-09-30 15:49:01 -0500

    refactor web component traversal and expectations

--- tests/e2e/specs/inputs.js
+++ tests/e2e/specs/inputs.js
@@ -5,10 +5,10 @@ describe('Checkbox', () => {
     cy.visit('http://localhost:8080')
   })
   it('should have default value', () => {
-    cy.get('ion-checkbox').its('checked').should('equal', false)
+    cy.get('ion-checkbox').find("input[type=hidden]").invoke('val').should("be.empty")
 
-    cy.get('#set').click();
+    cy.get('ion-checkbox').find('button').click();
 
-    cy.get('ion-checkbox').its('checked').should('equal', true)
+    cy.get('ion-checkbox').find("input[type=hidden]").invoke('val').should("eq", "on")
   })
 })
